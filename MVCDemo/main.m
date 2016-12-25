29a5b4e8c0720ed60a2ee90e1910625d415367d7
taitanxiami <taitanxiami@gmail.com>
2016-12-25 20:41:54 +0800

    Initial Commit

---
+++ MVCDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MVCDemo
+//
+//  Created by dianda on 2016/12/25.
+//  Copyright © 2016年 dianda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
